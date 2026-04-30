fe3bd1a31a96cb30f5ad5102c25b1e71adce80f4
rayssalsantos20 <rayssalayannegomes@gmail.com>
2026-04-30 16:54:03 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "calculadora-escolar-v3";
+const CACHE_NAME = "calculadora-escolar-v10";
 
 const urlsToCache = [
 
@@ -15,6 +15,8 @@ const urlsToCache = [
 
 self.addEventListener("install", event => {
 
+self.skipWaiting();
+
 event.waitUntil(
 
 caches.open(CACHE_NAME)
@@ -30,17 +32,34 @@ return cache.addAll(urlsToCache);
 });
 
 
+self.addEventListener("activate", event => {
+
+event.waitUntil(
+
+caches.keys().then(keys => {
+
+return Promise.all(
+
+keys.filter(key => key !== CACHE_NAME)
+
+.map(key => caches.delete(key))
+
+);
+
+})
+
+);
+
+});
+
+
 self.addEventListener("fetch", event => {
 
 event.respondWith(
 
-caches.match(event.request)
+fetch(event.request)
 
-.then(response => {
-
-return response || fetch(event.request);
-
-})
+.catch(() => caches.match(event.request))
 
 );
 
