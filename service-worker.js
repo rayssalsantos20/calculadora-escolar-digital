fb68ef4884b9fce58206421f907aaf0f401dde0c
rayssalsantos20 <rayssalayannegomes@gmail.com>
2026-04-30 15:08:55 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,55 +1,47 @@
-const CACHE_NAME = "calculadora-escolar-v4";
+const CACHE_NAME = "calculadora-escolar-v2";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+
+"./",
+"./index.html",
+"./style.css",
+"./script.js",
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png"
+
 ];
 
 
 self.addEventListener("install", event => {
 
-  self.skipWaiting();
+event.waitUntil(
 
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+caches.open(CACHE_NAME)
 
-});
+.then(cache => {
 
+return cache.addAll(urlsToCache);
 
-self.addEventListener("activate", event => {
+})
 
-  event.waitUntil(
-    caches.keys().then(keys => {
-
-      return Promise.all(
-        keys.map(key => {
-
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-
-        })
-      );
-
-    })
-  );
+);
 
 });
 
 
 self.addEventListener("fetch", event => {
 
-  event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request)
-    )
-  );
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response => {
+
+return response || fetch(event.request);
+
+})
+
+);
 
 });
